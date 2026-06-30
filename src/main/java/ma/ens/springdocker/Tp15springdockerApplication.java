3b2c2b034bad3f80f6496dd25aa583196c0c9e51
khadija sajid <khadija.sajid21@gmail.com>
2026-06-30 13:21:00 +0100

    first commit

---
+++ src/main/java/ma/ens/springdocker/Tp15springdockerApplication.java
@@ -0,0 +1,13 @@
+package ma.ens.springdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp15springdockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp15springdockerApplication.class, args);
+    }
+
+}
